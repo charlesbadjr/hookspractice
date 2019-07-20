34291a3ecf2575d3e03611ef223b115eabf269cc
Charlie <charlesbadjr@gmail.com>
2019-07-20 15:51:29 -0600

    lunch time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import charPicker from './components/CharPicker';
+import Character from './components/Character';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  state = {
+    selectedCharacter: 1,
+    side: 'light',
+    destroyed: false
+  };
+  
+  sideHandler = side => {
+    this.setState({ side: side });
+  };
+
+  charSelectHandler = event => {
+    const charId = event.target.value;
+    this.setState({ selectedCharacter: charId });
+  };
+
+  destructureHandler = () => {
+    this.setState({ destroyed: true });
+  };
+
+  render(){
+    let content = (
+      <React.Fragment>
+        <CharPicker
+           side={this.state.side}
+           selectedChar={this.state.selectedCharacter}
+           onCharSelect={this.charSelectHandler}
+        />
+        <Character selectedChar={this.state.selectedCharacter} />
+        <button onClick={this.sideHandler.bind(this, 'dark')}> Dard Side</button>
+        {this.state.side ==='dark' && (
+          <button onClick={this.destructionHandler}> Destroy! </button>
+        )} 
+      </React.Fragment>
+    );
+
+    if (this.state.destroyed) {
+      content = <h1> Total Destruction! </h1>;
+    }
+    return content;
+  }  
 }
 
 export default App;
---
+++ src/components/Summary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Sumary.css';
+
+
+const Summary = props => {
+    return (
+        <div>
+            <h1> {props.name} </h1>
+            <p>
+                Gender: <span className="sumary__output"> {props.gender} </span>
+            </p>
+            <p>
+                Height: <span className="sumary__output"> {props.height} </span>
+            </p>
+            <p>
+                Hair Color / Skin Color: {' '}
+                 <span className="sumary__output"> {props.hairColor} </span>/{ ' ' }
+                 <span className="sumary__output"> {props.skinColor} </span>
+            </p>
+            <p>
+                Appears in # Movies:{ ' ' }
+                <span className="summary__output"> {props.movieCount} </span>
+            </p>
+        </div>
+    );
+};
+
+export default Summary;
